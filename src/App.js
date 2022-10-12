4a1e2c6255291dcc7a9dfde935fca8c9d5ee4924
alifshah <alifhasanshah@gmail.com>
2022-10-12 22:11:45 +0600

    small changes done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
-import List from './pages/list/List'
+import List from './pages/list/List';
 import Login from './pages/login/Login'
 import Single from './pages/single/Single'
 import New from './pages/new/New'
