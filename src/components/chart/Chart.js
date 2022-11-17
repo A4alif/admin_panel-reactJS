05215c172d72e579860c99ae2bc1f5635a8797b8
alifshah <alifhasanshah@gmail.com>
2022-11-17 15:49:52 +0600

    chart complete

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,14 +1,79 @@
-import React from 'react'
-import './chart.scss'
+import React from "react";
+import "./chart.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Chart = () => {
+  const data = [
+    {
+      name: "January",
+      Total: 1500,
+    },
+    {
+      name: "February",
+      Total: 2200,
+    },
+    {
+      name: "March",
+      Total: 600,
+    },
+    {
+      name: "April",
+      Total: 1800,
+    },
+    {
+      name: "May",
+      Total: 900,
+    },
+    {
+      name: "June",
+      Total: 2000,
+    },
+    {
+      name: "July",
+      Total: 700,
+    },
+  ];
   return (
     <>
-    <div className='chart'>
-        Chart
-    </div>
-    </>
-  )
-}
+      <div className="chart">
+        <div className="title">Last Five Month Revenue</div>
+        <ResponsiveContainer width="100%" aspect={2 / 1}>
+          <AreaChart
+            width={730}
+            height={250}
+            data={data}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="name" />
 
-export default Chart
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="Total"
+              stroke="#8884d8"
+              fillOpacity={1}
+              fill="url(#total)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    </>
+  );
+};
+
+export default Chart;
